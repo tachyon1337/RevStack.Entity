ef36d50b69d6a14d4c02e3ea5512255a890696ac
Scott <dev@misonline.biz>
2015-10-08 16:16:19 -0400

    Initial Commit

---
+++ RevStack.Entity/Repository/EntityRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using RevStack.Pattern;
+using System.Linq.Expressions;
+
+namespace RevStack.Entity
+{
+    public class EntityRepository<TEntity, TKey> : IRepository<TEntity, TKey> where TEntity : class, IEntity<TKey>
+    {
+        private DbContext _context;
+        public EntityRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<TEntity> Get()
+        {
+            return _context.Set<TEntity>();
+        }
+
+        public IQueryable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _context.Set<TEntity>().Where(predicate).AsQueryable();
+        }
+
+        public TEntity Add(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+            return entity;
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            _context.Entry(entity).State= EntityState.Modified;
+            return entity;
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+        }
+
+    }
+}
---
+++ RevStack.Entity/UnitOfWork/EntityUnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using RevStack.Pattern;
+
+namespace RevStack.Entity
+{
+    public class EntityUnitOfWork<TEntity, TKey> : IUnitOfWork<TEntity, TKey> where TEntity : class, IEntity<TKey>
+    {
+        private DbContext _context;
+        public EntityUnitOfWork(DbContext context)
+        {
+            _context = context;
+        }
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public Task CommitAsync()
+        {
+            Commit();
+            return Task.FromResult(true);
+        }
+    }
+}
