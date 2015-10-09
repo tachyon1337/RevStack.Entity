69f622d8d50d5a27a8223df2f6322df983a739d5
Scott <dev@misonline.biz>
2015-10-09 07:14:18 -0400

    IDisposable Update

--- RevStack.Entity/UnitOfWork/EntityUnitOfWork.cs
+++ RevStack.Entity/UnitOfWork/EntityUnitOfWork.cs
@@ -22,5 +22,31 @@ namespace RevStack.Entity
             Commit();
             return Task.FromResult(true);
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    if(_context !=null)
+                    {
+                        _context.Dispose();
+                    }
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
